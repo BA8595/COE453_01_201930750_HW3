a238bb6256e2ae5c228a1791678cafc870078f54
BA8595 <161141899+BA8595@users.noreply.github.com>
2024-03-24 21:35:21 +0000

    Cloud_functions

---
+++ Cloud_Functions/Mul.js
@@ -0,0 +1,14 @@
+const functions = require('@google-cloud/functions-framework');
+
+functions.http('helloHttp', (req, res) => {
+  var x = parseInt(req.query.X || req.body.X) 
+  var y = parseInt(req.query.Y || req.body.Y) 
+
+  let z = x * y ;
+  res.send({
+    "X":x,
+    "Y":y,
+    "Result":z
+  })
+
+});
